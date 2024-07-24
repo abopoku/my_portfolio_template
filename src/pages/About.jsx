74c67691ee599f38469ddde7bd50b1cec64a6359
abopoku <aba.opoku@gmail.com>
2024-07-23 22:24:26 -0500

    updated some information and images.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,14 @@
 import { NavLink } from 'react-router-dom';
-import PlaceholderImage from '../assets/placeholder_image.png';
+import PlaceholderImage from '../assets/linkedin_profile.jpg';
 
 // Replace these variables with your information
-const firstName = 'Replace with your first name';
-const jobTitle = 'Replace with your title';
-const company = 'Replace with your company name';
-const mainDuty = 'Replace with your main duty';
-const jobLocation = 'Replace with your job location';
-const briefJobDescription = 'Replace with a brief description of your job';
-const careerObjective = 'Replace with your career objective';
+const firstName = 'Aba';
+const jobTitle = 'Full Stack Developer';
+const company = 'Npower';
+const mainDuty = 'Trainee';
+const jobLocation = 'Remote Dallas, TX';
+const briefJobDescription = 'Completed an intensive 20-week Full Stack Developer training program, gaining hands-on experience in both front-end and back-end development technologies, including HTML, CSS, and JavaScript. Developed proficiency in building responsive and interactive user interfaces using popular front-end frameworks such as React.js, ensuring a seamless user experience across various devices. Acquired essential back-end development skills through practical training in server-side scripting with Node.js, utilizing Express.js for efficient routing and building scalable web applications. Demonstrated the ability to work with databases by mastering fundamental database management concepts and hands-on experience with MongoDB, ensuring effective storage and retrieval of data. Collaborated with peers on real-world projects, applying Agile development methodologies, version control using Git, and participating in regular code reviews, fostering a collaborative environment.';
+const careerObjective = 'Ive spent over 5 years as a business professional, focusing on how to blend business strategies with best world class operational process to enhance performance and keep customers happy. Ive worn many hats, from founding and leading Opoku advisors to managing teams and coordinating projects at companies like SVI, Chewy, and Jones Lang Lasalle. My leadership journey has mainly been in the healthcare space, where I was always drawn to using the technological aspects to improve operational performance. What gets me excited is finding innovative ways to solve problems, improve processes, working with teams, diving into data to drive decisions, and insuring projects run smoothly from start to finish. I am currently a software developer trainee in a program with Npower and I plan to utilize the experience along with a scrum master certification to enhance my skills ultimately to lay down the foundation to be a part of harnessing the continued evolution of technology.';
 const personalLife = 'Replace with a brief description of your personal life';
 
 const About = () => {
