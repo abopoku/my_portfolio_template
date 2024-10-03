f79087e789b8bfbea6b04efc8b0de096c2aa3547
abopoku <aba.opoku@gmail.com>
2024-10-03 15:57:12 -0500

    "updated resume link"

--- src/components/DownloadResume.jsx
+++ src/components/DownloadResume.jsx
@@ -3,7 +3,7 @@ import { FaDownload } from "react-icons/fa";
 const DownloadResume = () => {
   return (
     // Replace the href with the a link to your resume and replace the download attribute with your name
-    <a href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:80bdcb4e-25db-4c75-805c-efb598ac0b8e" download="Aba Opoku_Resume.pdf" className="flex items-center">
+    <a href="https://acrobat.adobe.com/id/urn:aaid:sc:va6c2:65da0a9b-ee8e-4ae7-aaf3-f2e5d35429e2" download="Aba Opoku_Resume.pdf" className="flex items-center">
       <p className="mr-2">Download Resume</p>
       <FaDownload />
     </a>
